65e1a431fc6c647097a53341d9af735984c8a139
4madeus <edissonman255@gmail.com>
2024-11-11 21:27:18 +0300

    usetState data

--- src/components/MainInfo/MainInfo.tsx
+++ src/components/MainInfo/MainInfo.tsx
@@ -5,30 +5,34 @@ import photo_test from "../../../assets/photo.jpg";
 import Union from "../../../assets/Union.svg?react";
 import Share from "../../../assets/Share.svg?react";
 import Message from "../../../assets/Message.svg?react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function MainInfo() {
+  const [data, setData] = useState({
+    name: "",
+    tag: "",
+    description: "",
+    region: "",
+  });
   useEffect(() => {
     fetch(`http://localhost:8080/user/840986267`)
       .then((response) => response.json())
       // 4. Setting *dogImage* to the image url that we received from the response above
-      .then((data) => console.log(data))
+      .then((data) => setData(data))
       .catch((e) => console.log(e));
   }, []);
   return (
     <div className="main-info">
       <img src={photo_test} alt="" />
-      <div className="name">Аня Мельник</div>
-      <div className="nick">@amelnikcom</div>
-      <div className="desc">
-        Креативный дизайнер, основательница школы Цитадель и студии Dofamin
-      </div>
+      <div className="name">{data.name}</div>
+      <div className="nick">{data.tag}</div>
+      <div className="desc">{data.description}</div>
       <hr className="separator" />
       <div className="location">
         <div className="svg">
           <Union />
         </div>
-        <div>Ташкент, Узбекистан</div>
+        <div>{data.region}</div>
       </div>
       <div className="main-buttons">
         <Button>
