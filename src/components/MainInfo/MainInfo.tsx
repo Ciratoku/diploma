f84d52d20eea628e16b5c7555d43ac40985893f1
4madeus <edissonman255@gmail.com>
2024-11-11 21:56:04 +0300

    photo

--- src/components/MainInfo/MainInfo.tsx
+++ src/components/MainInfo/MainInfo.tsx
@@ -1,11 +1,13 @@
+import { useEffect, useState } from "react";
+
 import "./MainInfo.css";
 import Button from "../Button/Button";
+import { b64toBlob } from "../Utils/Utils";
 
 import photo_test from "../../../assets/photo.jpg";
 import Union from "../../../assets/Union.svg?react";
 import Share from "../../../assets/Share.svg?react";
 import Message from "../../../assets/Message.svg?react";
-import { useEffect, useState } from "react";
 
 function MainInfo() {
   const [data, setData] = useState({
@@ -13,17 +15,26 @@ function MainInfo() {
     tag: "",
     description: "",
     region: "",
+    photo:
+      "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==",
   });
+  const [img, setImg] = useState("");
   useEffect(() => {
     fetch(`http://localhost:8080/user/840986267`)
       .then((response) => response.json())
       // 4. Setting *dogImage* to the image url that we received from the response above
-      .then((data) => data && setData(data))
+      .then((data) => {
+        setData(data);
+        const contentType = "image/png";
+        const blob = b64toBlob(data.photo, contentType);
+        const blobUrl = URL.createObjectURL(blob);
+        setImg(blobUrl);
+      })
       .catch((e) => console.log(e));
   }, []);
   return (
     <div className="main-info">
-      <img src={photo_test} alt="" />
+      <img src={img} alt="" />
       <div className="name">{data.name}</div>
       <div className="nick">{data.tag}</div>
       <div className="desc">{data.description}</div>
