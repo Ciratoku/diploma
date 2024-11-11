915f55b366983cddf5131071639284052efd064f
4madeus <edissonman255@gmail.com>
2024-11-11 16:44:22 +0300

    Block

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import "./Block.css";
 
 function Block() {
-  return <div>Block</div>;
+  const desc: string[] = [
+    "🏆 19 попаданий в галерею Made on Tilda",
+    "🏆 10 международных наград Awwwards HM",
+  ];
+  return (
+    <div className="block-wrapper">
+      <p>описание</p>
+      <div className="block">
+        {desc.map((d, i) => (
+          <p key={i}>{d}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Block;
