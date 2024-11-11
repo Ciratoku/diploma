18b770a8cc7b60f819b1c2edc86bb5b97e6ad52a
4madeus <edissonman255@gmail.com>
2024-11-11 20:45:16 +0300

    fetch data and print

--- src/components/MainInfo/MainInfo.tsx
+++ src/components/MainInfo/MainInfo.tsx
@@ -5,8 +5,16 @@ import photo_test from "../../../assets/photo.jpg";
 import Union from "../../../assets/Union.svg?react";
 import Share from "../../../assets/Share.svg?react";
 import Message from "../../../assets/Message.svg?react";
+import { useEffect } from "react";
 
 function MainInfo() {
+  useEffect(() => {
+    fetch(`http://localhost:8080/user/840986267`)
+      .then((response) => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then((data) => console.log(data))
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <div className="main-info">
       <img src={photo_test} alt="" />
