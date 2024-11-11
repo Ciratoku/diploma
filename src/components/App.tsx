6ac20c81134afb46fa0f5ec10ffc5bdf479734ac
4madeus <edissonman255@gmail.com>
2024-11-11 13:29:41 +0300

    rm unnecessary imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,5 @@
-import { useLaunchParams, miniApp, useSignal } from '@telegram-apps/sdk-react';
-import { AppRoot } from '@telegram-apps/telegram-ui';
-import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
-
-import { routes } from '@/navigation/routes.tsx';
+import { useLaunchParams, miniApp, useSignal } from "@telegram-apps/sdk-react";
+import { AppRoot } from "@telegram-apps/telegram-ui";
 
 export function App() {
   const lp = useLaunchParams();
