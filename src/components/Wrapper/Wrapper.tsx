e27219db049862ee7d8094c9df6f7bd6941e86c1
4madeus <edissonman255@gmail.com>
2024-11-11 16:44:49 +0300

    move props to another file

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,22 +1,5 @@
-import { ReactElement, ReactPortal } from "react";
+import { Props } from "@/types";
 import "./Wrapper.css";
-
-type ReactText = string | number;
-type ReactChild = ReactElement | ReactText;
-
-interface ReactNodeArray extends Array<ReactNode> {}
-type ReactFragment = ReactNodeArray;
-type ReactNode =
-  | ReactChild
-  | ReactFragment
-  | ReactPortal
-  | boolean
-  | null
-  | undefined;
-
-type Props = {
-  children: ReactNode;
-};
 
 function Wrapper({ children }: Props) {
   return <div className="wrapper">{children}</div>;
